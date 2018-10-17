406fc8bdd506566806aaae7e653c14486865d9dd
dangkhoa2708 <tdkhoa27081997@gmail.com>
2018-10-17 13:15:53 +0700

    edit MessageImage

--- src/MessageImage.js
+++ src/MessageImage.js
@@ -35,8 +35,7 @@ const styles = StyleSheet.create({
   image: {
     width: 150,
     height: 100,
-    borderRadius: 13,
-    margin: 3,
+    borderRadius: 8,
     resizeMode: 'cover',
   },
   imageActive: {
