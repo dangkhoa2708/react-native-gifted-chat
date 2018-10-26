29a29d367d176a6ce7a56d7572bedfe7446cdc94
dangkhoa2708 <tdkhoa27081997@gmail.com>
2018-10-26 15:54:12 +0700

    Update MessageImage.js

--- src/MessageImage.js
+++ src/MessageImage.js
@@ -2,7 +2,7 @@
 
 import PropTypes from 'prop-types';
 import React from 'react';
-import { Image, StyleSheet, View, ViewPropTypes, Dimensions, TouchableOpacity } from 'react-native';
+import { Image, StyleSheet, View, ViewPropTypes, Dimensions, TouchableOpacity, Text } from 'react-native';
 import Lightbox from 'react-native-lightbox';
 
 const { width, height } = Dimensions.get('window')
@@ -42,6 +42,11 @@ export default function MessageImage({
       activeProps={{
         style: styles.imageActive,
       }}
+      renderHeader={close => (
+        <TouchableOpacity onPress={close} style={styles.closeHolder}>
+          <Text style={styles.closeButton}>X</Text>
+        </TouchableOpacity>
+      )}
       renderContent={() => {
         return (
           <Image
@@ -81,20 +86,18 @@ const styles = StyleSheet.create({
     flex: 1,
     resizeMode: 'cover',
   },
-  close: {
-    width: 30,
-    height: 30
+  closeButton: {
+    fontSize: 20,
   },
   closeHolder: {
-    width,
-    position: 'absolute',
-    top: 5,
-    zIndex: 100000,
-    left: 5,
-    width: 50,
-    height: 50,
-    alignItems: 'center',
-    justifyContent: 'center'
+    width: 32,
+    height: 32,
+    marginTop: 15,
+    marginLeft: 15,
+    borderRadius: 16,
+    backgroundColor: '#878585',
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 });
 
